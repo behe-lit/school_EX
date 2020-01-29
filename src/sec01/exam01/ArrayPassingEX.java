d179f6fe2aa5da461a212a638c9e512c0e1d8d85
it <it@DESKTOP-QOQQD1R>
2020-01-29 16:15:09 +0900

    1단계

---
+++ src/sec01/exam01/ArrayPassingEX.java
@@ -0,0 +1,18 @@
+package sec01.exam01;
+
+public class ArrayPassingEX {
+	
+	static void printCharArray(char[] a) {
+		
+	}
+	
+	static void replaceSpace() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
