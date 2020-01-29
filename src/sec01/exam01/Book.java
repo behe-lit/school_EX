5003c8646a4df3cd6a81ac1e8439fe54ce625e4a
it <it@DESKTOP-QOQQD1R>
2020-01-29 17:04:29 +0900

    연습커밋

--- src/sec01/exam01/Book.java
+++ src/sec01/exam01/Book.java
@@ -7,25 +7,27 @@ public class Book {
 		
 	
 		static Scanner input = new Scanner(System.in);
-	
+		//필드
 		String title, author;
 		
+		//생성자
 		Book(String title, String author) {
 			this.title = title;
 			this.author = author;
 		}
 		
 		public static void main(String[] args) {
-			Book[] book = new Book[2];
+			
+			Book[] book = new Book[2]; //레퍼런스를 2개 생성
 			
 			for(int i=0; i<book.length; i++) {
 
 				System.out.print("제목>>");
-				String title = input.nextLine();
+				String title = input.nextLine(); //입력 변수, 필드값과 독립적?
 				System.out.print("저자>>");
 				String author = input.nextLine();
 				
-				book[i] = new Book(title, author);
+				book[i] = new Book(title, author); //전달 인자값을 여기서 생성
 
 			}
 			
