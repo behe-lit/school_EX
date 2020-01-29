87b8f344e792b57296a85678fbc4cf86c242b90b
it <it@DESKTOP-QOQQD1R>
2020-01-29 16:09:19 +0900

    최종

--- src/sec01/exam01/Book.java
+++ src/sec01/exam01/Book.java
@@ -7,9 +7,9 @@ public class Book {
 		
 		static Scanner input = new Scanner(System.in);
 	
-		static String title, author;
+		String title, author;
 		
-		Book(String title, String athor) {
+		Book(String title, String author) {
 			this.title = title;
 			this.author = author;
 		}
@@ -19,14 +19,19 @@ public class Book {
 			
 			for(int i=0; i<book.length; i++) {
 				System.out.print("제목>>");
-				Book.title = input.nextLine();
+				String title = input.nextLine();
 				System.out.print("저자>>");
-				Book.author = input.nextLine();
-				book[i] = new Book(title,author);
+				String author = input.nextLine();
+				
+				book[i] = new Book(title, author);
 			}
 			
 			for(int i=0; i<book.length; i++) {
 				System.out.print(book[i].title +", "+ book[i].author + ", ");
+			
+				input.close();
 			}
+		
 		}
+	
 	}
