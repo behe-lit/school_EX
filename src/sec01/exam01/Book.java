228c33ed9f0c33f01affbba8aa85eb5ad6826ff7
it <it@DESKTOP-QOQQD1R>
2020-01-30 09:59:03 +0900

    BufferedReader로 입력

--- src/sec01/exam01/Book.java
+++ src/sec01/exam01/Book.java
@@ -1,12 +1,14 @@
 package sec01.exam01;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Book {
 		
 	
-		static Scanner input = new Scanner(System.in);
+		static Scanner sc = new Scanner(System.in);
 		//필드
 		String title, author;
 		
@@ -16,16 +18,19 @@ public class Book {
 			this.author = author;
 		}
 		
-		public static void main(String[] args) {
+		public static void main(String[] args) throws IOException {
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
 			
 			Book[] book = new Book[2]; //레퍼런스를 2개 생성
 			
 			for(int i=0; i<book.length; i++) {
 
 				System.out.print("제목>>");
-				String title = input.nextLine(); //입력 변수, 필드값과 독립적?
+				String title = br.readLine(); //입력 변수, 필드값과 독립적?
 				System.out.print("저자>>");
-				String author = input.nextLine();
+				String author = br.readLine();
 				
 				book[i] = new Book(title, author); //전달 인자값을 여기서 생성
 
@@ -34,7 +39,7 @@ public class Book {
 			for(int i=0; i<book.length; i++) {
 				System.out.print(book[i].title +", "+ book[i].author + ", ");
 			
-				input.close(); //연습커밋
+				br.close(); //연습커밋
 			}
 		
 		}
