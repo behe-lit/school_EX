bb3daed79a95114f99058ac20d7ae9d0b55d41c2
admin <admin@DESKTOP-PIGFCFI>
2020-02-13 18:03:54 +0900

    커밋

---
+++ src/sec06/exam05/WrapperEx.java
@@ -0,0 +1,36 @@
+package sec06.exam05;
+
+public class WrapperEx {
+	public static void main(String[] args) {
+		/*
+		 * System.out.println(Character.toLowerCase('A'));
+		 * 
+		 * char c1='4', c2='F'; if(Character.isDigit(c1)) System.out.println(c1+
+		 * "는 숫자"); if(Character.isAlphabetic(c2)) System.out.println(c2+ "는 영문자");
+		 */
+		
+		/*
+		 * System.out.println(Integer.parseInt("-123"));
+		 * System.out.println(Integer.toHexString(28));
+		 * System.out.println(Integer.toBinaryString(28));
+		 * System.out.println(Integer.bitCount(28));
+		 */
+		
+		
+		Double d = Double.valueOf(3.14);
+		System.out.println(d);
+		System.out.println(d.toString());
+		
+		System.out.println(System.identityHashCode(d));
+		System.out.println(System.identityHashCode(d.toString()));
+		
+		
+		boolean b = (4>3);
+		System.out.println(Boolean.toString(b));
+		System.out.println(Boolean.parseBoolean("false"));
+		System.out.println(b);
+		
+		
+		
+	}
+}
